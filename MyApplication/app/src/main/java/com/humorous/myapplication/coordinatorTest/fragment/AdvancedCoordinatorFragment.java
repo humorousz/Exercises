cfe4eef19f096fe8632f91c87d524cd8382ab984
张智全 <zhangzhiquan@alibaba-inc.com>
2017-06-15 19:14:52 +0800

    练习CollapsingToolbarLayout

--- MyApplication/app/src/main/java/com/humorous/myapplication/coordinatorTest/fragment/AdvancedCoordinatorFragment.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/coordinatorTest/fragment/AdvancedCoordinatorFragment.java
@@ -2,6 +2,7 @@ package com.humorous.myapplication.coordinatorTest.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import com.humorousz.uiutils.view.BaseFragment;
 
 public class AdvancedCoordinatorFragment extends BaseFragment {
     private ImageView mImage;
+    private Toolbar mToolBar;
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layout_test_advanced_coordinator_fragment,container,false);
@@ -24,6 +26,8 @@ public class AdvancedCoordinatorFragment extends BaseFragment {
     @Override
     public void initView(View root) {
 //        mContainer = (FrameLayout) root.findViewById(R.id.advanced_container);
+        mToolBar = (Toolbar) root.findViewById(R.id.toolbar);
+        mToolBar.setTitle("哈哈哈哈");
         mImage = (ImageView) root.findViewById(R.id.advance_image);
         FragmentTransaction tr = getChildFragmentManager().beginTransaction();
         tr.add(R.id.advanced_container,new TestListFragment());
