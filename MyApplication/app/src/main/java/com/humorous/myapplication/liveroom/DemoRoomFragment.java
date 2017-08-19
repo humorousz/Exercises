342a15d0a31f2245eb8cfcafe7b37a6d216d2da4
张智全 <zhangzhiquan@alibaba-inc.com>
2017-08-19 23:37:23 +0800

    commit into room

--- MyApplication/app/src/main/java/com/humorous/myapplication/liveroom/DemoRoomFragment.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/liveroom/DemoRoomFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.humorous.myapplication.R;
+import com.humorous.myapplication.liveroom.intoRoom.IntoRoomAnimatorView;
 import com.humorousz.uiutils.view.BaseFragment;
 
 /**
@@ -13,6 +15,8 @@ import com.humorousz.uiutils.view.BaseFragment;
  */
 
 public class DemoRoomFragment extends BaseFragment {
+    IntoRoomAnimatorView intoRoomAnimatorView;
+    FrameLayout mContainer;
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layoyt_fragment_demo_room,container,false);
@@ -20,6 +24,20 @@ public class DemoRoomFragment extends BaseFragment {
 
     @Override
     public void initView(View root) {
+        mContainer = (FrameLayout) root.findViewById(R.id.into_room_container);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        intoRoomAnimatorView = new IntoRoomAnimatorView(getContext());
+        mContainer.addView(intoRoomAnimatorView);
+        intoRoomAnimatorView.setData("张智全 进入了房间");
+        mContainer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intoRoomAnimatorView.setData("张智全 又进入了房间");
+            }
+        },6000);
     }
 }
---
+++ MyApplication/app/src/main/java/com/humorous/myapplication/liveroom/intoRoom/IntoRoomAnimatorView.java
@@ -0,0 +1,104 @@
+package com.humorous.myapplication.liveroom.intoRoom;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.humorous.myapplication.R;
+import com.humorousz.commonutils.log.Logger;
+
+/**
+ * Created by zhangzhiquan on 2017/8/19.
+ */
+
+public class IntoRoomAnimatorView extends LinearLayout implements Animation.AnimationListener {
+    private static final String TAG = "IntoRoomAnimatorView";
+    private TextView mTextView;
+    private FrameLayout mTextContainer;
+    private ImageView mStar;
+    private Context mContext;
+    private Animation inAnimation,outAnimation;
+    private OnAnimationStateListener mListener;
+    private boolean isRunning;
+    public IntoRoomAnimatorView(Context context) {
+        this(context,null);
+    }
+
+    public IntoRoomAnimatorView(Context context, AttributeSet attrs) {
+        this(context, attrs,-1);
+    }
+
+    public IntoRoomAnimatorView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        init();
+
+    }
+
+    private void init(){
+        LayoutInflater.from(mContext).inflate(R.layout.layout_into_room_view,this);
+        mTextContainer = (FrameLayout) findViewById(R.id.text_container);
+        mStar = (ImageView) findViewById(R.id.star_img);
+        mTextView = (TextView) findViewById(R.id.text_into);
+        inAnimation = AnimationUtils.loadAnimation(mContext,R.anim.into_right_to_left);
+        inAnimation.setFillAfter(true);
+        inAnimation.setAnimationListener(this);
+        outAnimation = AnimationUtils.loadAnimation(mContext,R.anim.into_out);
+        outAnimation.setFillAfter(true);
+        outAnimation.setAnimationListener(this);
+        mStar.setImageResource(R.drawable.aa);
+        mStar.clearAnimation();
+    }
+
+    public void setData(CharSequence data){
+        if(isRunning)
+            return;
+        mTextView.setText(data);
+        AnimationDrawable anim = (AnimationDrawable) mStar.getDrawable();
+        anim.start();
+        mTextContainer.startAnimation(inAnimation);
+    }
+
+    public void setOnAnimationStateListener(OnAnimationStateListener listener){
+        mListener = listener;
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        if(mListener != null){
+            mListener.onStart();
+        }
+        isRunning = true;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if(animation == inAnimation){
+            mTextContainer.startAnimation(outAnimation);
+        }else if(animation == outAnimation){
+            AnimationDrawable anim = (AnimationDrawable) mStar.getDrawable();
+            anim.stop();
+            if(mListener != null){
+                mListener.onEnd();
+            }
+            isRunning = false;
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    public interface OnAnimationStateListener{
+        void onStart();
+        void onEnd();
+    }
+}
