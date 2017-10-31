117088316ae7f62d325c6a6ba1e7e5b1b55239bb
张智全 <zhangzhiquan@alibaba-inc.com>
2017-10-31 16:22:03 +0800

    delete file

--- MyApplication/app/src/main/java/com/humorous/myapplication/vectorDrawableTest/GuardView.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/vectorDrawableTest/GuardView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.humorous.myapplication.R;
 
@@ -13,8 +15,21 @@ import com.humorous.myapplication.R;
  */
 
 public class GuardView extends RelativeLayout {
-    private ViewGroup root;
     private Context mContext;
+    private ImageView mLightBg;
+    private ImageView mStarBg;
+    private ImageView mBigStarBg;
+    private ImageView mLeftWing;
+    private ImageView mRightWing;
+    private ImageView mPlant;
+    private ViewGroup mIconContainer;
+    private TextView  mUserNameText;
+    private TextView  mUserDescText;
+    private ViewGroup mTypeIconContainer;
+    private ImageView mTypeIcon;
+    private ImageView mTypeIconStar;
+    private AroundLightView mAroundLightView;
+
     public GuardView(Context context) {
         this(context,null);
     }
@@ -27,10 +42,31 @@ public class GuardView extends RelativeLayout {
         super(context, attrs, defStyleAttr);
         mContext = context;
         init();
+        setAllViewGone();
     }
 
     private void init(){
         LayoutInflater inflater = LayoutInflater.from(mContext);
         inflater.inflate(R.layout.layout_gurad_view,this,true);
+        mLightBg = (ImageView) findViewById(R.id.guard_bg);
+        mStarBg = (ImageView) findViewById(R.id.guard_star_bg);
+        mBigStarBg = (ImageView) findViewById(R.id.guard_big_star_bg);
+        mLeftWing = (ImageView) findViewById(R.id.left_wing);
+        mRightWing = (ImageView) findViewById(R.id.right_wing);
+        mPlant = (ImageView) findViewById(R.id.plant);
+        mIconContainer = (ViewGroup) findViewById(R.id.icon_container);
+        mUserNameText = (TextView) findViewById(R.id.text_user);
+        mUserDescText = (TextView) findViewById(R.id.text_desc);
+        mTypeIconContainer = (ViewGroup) findViewById(R.id.guard_type_icon_container);
+        mTypeIcon = (ImageView) findViewById(R.id.guard_type_icon);
+        mTypeIconStar = (ImageView) findViewById(R.id.guard_type_icon_star);
+        mAroundLightView =  (AroundLightView) findViewById(R.id.guard_around_light);
+    }
+
+    private void setAllViewGone(){
+        int count = getChildCount();
+        for(int i = 0; i < count;i++){
+            getChildAt(i).setVisibility(INVISIBLE);
+        }
     }
 }
