ba5ea9b45f34966744f834b329c484fa92243655
zhangzhiquan <zhanzhiquan@alibaba-inc.com>
2017-12-15 16:39:53 +0800

    shader translate

--- MyApplication/app/src/main/java/com/humorous/myapplication/liveroom/weidget/TestShaderView.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/liveroom/weidget/TestShaderView.java
@@ -1,19 +1,23 @@
 package com.humorous.myapplication.liveroom.weidget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
-import com.humorous.myapplication.R;
+import com.humorousz.uiutils.helper.UIUtils;
+
 
 /**
  * Created by zhangzhiquan on 2017/12/14.
@@ -21,6 +25,10 @@ import com.humorous.myapplication.R;
  */
 
 public class TestShaderView extends View {
+    private Paint mPaint;
+    private Shader mShader;
+    private ValueAnimator mTranslateAnimator;
+    private float mTranslate = 1000;
 
     public TestShaderView(Context context) {
         this(context,null);
@@ -32,17 +40,45 @@ public class TestShaderView extends View {
 
     public TestShaderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        mPaint = new Paint();
+        mTranslateAnimator = ValueAnimator.ofFloat(0, UIUtils.dip2px(250));
+        mTranslateAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float v = (float) animation.getAnimatedValue();
+                mTranslate = v;
+                invalidate();
+            }
+        });
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mTranslateAnimator.setInterpolator(new DecelerateInterpolator());
+        mTranslateAnimator.setDuration(2000);
+        mTranslateAnimator.setRepeatCount(5);
+        mTranslateAnimator.start();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint mPaint = new Paint();
-        Shader shader = new LinearGradient(0, 0, 200, 200,
-                new int[] { Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.WHITE },
-                new float[] { 0, 0.1F, 0.5F, 0.7F, 0.8F }, Shader.TileMode.REPEAT);
         // 设置shader
-        mPaint.setShader(shader);
-        canvas.drawRect(0, 0, 400, 400, mPaint);
+        if(mShader == null){
+            mShader = new LinearGradient(0,0,canvas.getWidth()/2, canvas.getHeight() ,
+                    new int[] {Color.parseColor("#01000000"),Color.parseColor("#11000000"),Color.parseColor("#01000000")},
+                    null,Shader.TileMode.CLAMP);
+        }
+        mPaint.setShader(mShader);
+        Matrix matrix = new Matrix();
+        matrix.setSkew(-0.5f,0,canvas.getWidth()/2,canvas.getHeight()/2);
+        canvas.setMatrix(matrix);
+        canvas.translate(mTranslate,0);
+        canvas.drawRect(0, 0, canvas.getWidth()/16, canvas.getHeight(), mPaint);
     }
 }
