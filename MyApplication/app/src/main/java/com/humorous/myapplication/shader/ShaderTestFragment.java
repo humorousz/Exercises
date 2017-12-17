801d4c888dd51bd2cf508a29b7377acfdd4afa63
张智全 <zhangzhiquan@alibaba-inc.com>
2017-12-17 18:16:20 +0800

    封装 maskView

--- MyApplication/app/src/main/java/com/humorous/myapplication/shader/ShaderTestFragment.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/shader/ShaderTestFragment.java
@@ -1,11 +1,17 @@
 package com.humorous.myapplication.shader;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.humorous.myapplication.R;
+import com.humorous.myapplication.shader.widget.MaskLoadingView;
+import com.humorousz.uiutils.helper.ImageLoaderHelper;
 import com.humorousz.uiutils.view.BaseFragment;
 
 /**
@@ -14,6 +20,8 @@ import com.humorousz.uiutils.view.BaseFragment;
  */
 
 public class ShaderTestFragment  extends BaseFragment{
+    private MaskLoadingView mTestMask,mImageMask;
+    private ImageView mImage;
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layout_fragment_test_shader,container,false);
@@ -21,6 +29,13 @@ public class ShaderTestFragment  extends BaseFragment{
 
     @Override
     public void initView(View root) {
+        mTestMask = root.findViewById(R.id.text_mask);
+        int[] colors = new int[]{Color.TRANSPARENT, Color.parseColor("#02ffffff"), Color.parseColor("#22ffffff"), Color.parseColor("#02ffffff"), Color.TRANSPARENT};
+        float[] position = new float[]{0.3f, 0.4f, 0.5f, 0.6f, 0.7f};
+        mTestMask.setColors(colors);
+        mTestMask.setPositions(position);
 
+        mImage = root.findViewById(R.id.shade_image);
+        ImageLoaderHelper.displayCircleImage("drawable://"+R.drawable.sssssss,mImage);
     }
 }
--- MyApplication/app/src/main/java/com/humorous/myapplication/shader/widget/MaskLoadingView.java
+++ MyApplication/app/src/main/java/com/humorous/myapplication/shader/widget/MaskLoadingView.java
@@ -8,15 +8,20 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Shader;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.humorous.myapplication.R;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 
 /**
  * Created by zhangzhiquan on 2017/12/14.
+ *
  * @author zhangzhiquan
  */
 
@@ -30,48 +35,69 @@ public class MaskLoadingView extends View {
     private long mDuration;
     private int repeatMode;
     private int repeatCount;
+    private int animMode;
+    private int shape;
     private boolean isNullPosition = false;
     private MaskLoadingCallBack mListener;
 
+    public static final int LEFT_TO_RIGHT = 1;
+    public static final int RIGHT_TO_LEFT = 2;
+
+    public static final int RECTANGLE = 1;
+    public static final int CIRCLE = 2;
+
+    /**
+     * @hide
+     */
+    @IntDef({LEFT_TO_RIGHT, RIGHT_TO_LEFT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface AnimMode {}
+
+    /**
+     * @hide
+     */
+    @IntDef({RECTANGLE,CIRCLE})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Shape{}
+
     public MaskLoadingView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public MaskLoadingView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,-1);
+        this(context, attrs, -1);
     }
 
     public MaskLoadingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initAttrs(context,attrs,defStyleAttr);
+        initAttrs(context, attrs, defStyleAttr);
         mPaint = new Paint();
-        post(new Runnable() {
-            @Override
-            public void run() {
-                init();
-            }
-        });
+
     }
 
 
-    private void initAttrs(Context context,AttributeSet attrs,int defStyleAttr){
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MaskLoadingView,defStyleAttr,0);
-        repeatCount = a.getInt(R.styleable.MaskLoadingView_repeatCount,-1);
-        repeatMode = a.getInt(R.styleable.MaskLoadingView_repeatMode,1);
-        mDuration = a.getInt(R.styleable.MaskLoadingView_duration,2000);
+    private void initAttrs(Context context, AttributeSet attrs, int defStyleAttr) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MaskLoadingView, defStyleAttr, 0);
+        repeatCount = a.getInt(R.styleable.MaskLoadingView_repeatCount, -1);
+        shape = a.getInt(R.styleable.MaskLoadingView_shape,RECTANGLE);
+        animMode = a.getInt(R.styleable.MaskLoadingView_animMode, LEFT_TO_RIGHT);
+        mDuration = a.getInt(R.styleable.MaskLoadingView_duration, 2000);
+        repeatMode = a.getInt(R.styleable.MaskLoadingView_repeatMode, 1);
         repeatCount = repeatCount < -1 ? -1 : repeatCount;
         repeatCount = repeatCount > 1 ? repeatCount - 1 : repeatCount;
         a.recycle();
     }
-    private void init(){
-        mTranslateAnimator = ValueAnimator.ofFloat(-getWidth(),getWidth());
-        mTranslateAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float v = (float) animation.getAnimatedValue();
-                mTranslate = v;
-                invalidate();
-            }
+
+    private void init() {
+        if (animMode == LEFT_TO_RIGHT) {
+            mTranslateAnimator = ValueAnimator.ofFloat(-getWidth(), getWidth());
+        } else if (animMode == RIGHT_TO_LEFT) {
+            mTranslateAnimator = ValueAnimator.ofFloat(getWidth(), -getWidth());
+        }
+        mTranslateAnimator.addUpdateListener((animation) -> {
+            float v = (float) animation.getAnimatedValue();
+            mTranslate = v;
+            invalidate();
         });
         mTranslateAnimator.setDuration(mDuration);
         mTranslateAnimator.setRepeatMode(repeatMode);
@@ -82,37 +108,44 @@ public class MaskLoadingView extends View {
 
     /**
      * 设置渐变颜色
+     *
      * @param colors
      */
-    public void setColors(int[] colors){
+    public void setColors(int[] colors) {
         mColors = colors;
     }
 
     /**
      * 设置渐变颜色位置
+     *
      * @param positions
      */
-    public void setPositions(float[] positions){
+    public void setPositions(float[] positions) {
         mPositions = positions;
     }
 
-    public void setNullPosition(boolean flag){
+    public void setNullPosition(boolean flag) {
         isNullPosition = flag;
     }
 
-    public void setCallbackListener(MaskLoadingCallBack listener){
+    public void setCallbackListener(MaskLoadingCallBack listener) {
         mListener = listener;
+    }
+
+    public void setAnimMode(@AnimMode int mode ){
+        animMode = mode;
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        post(() -> init());
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        if(mTranslateAnimator != null && mTranslateAnimator.isRunning()){
+        if (mTranslateAnimator != null && mTranslateAnimator.isRunning()) {
             mTranslateAnimator.cancel();
         }
     }
@@ -138,13 +171,16 @@ public class MaskLoadingView extends View {
     }
 
 
-    public interface MaskLoadingCallBack{
+    public interface MaskLoadingCallBack {
         /**
          * 获取Shader，如果使用时需要自定义
+         *
          * @param canvas 可以通过这个Canvas获取宽高
          * @return
          */
-        Shader getShader(Canvas canvas);
+        default Shader getShader(Canvas canvas) {
+            return null;
+        }
     }
 
 
