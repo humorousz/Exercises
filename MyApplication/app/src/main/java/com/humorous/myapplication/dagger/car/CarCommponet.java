01fd7b4fe2ed712bedd09e7eef28d907d2c4ea13
张智全 <407890754@qq.com>
2019-12-29 22:10:53 +0800

    dagger car wheel test

---
+++ MyApplication/app/src/main/java/com/humorous/myapplication/dagger/car/CarCommponet.java
@@ -0,0 +1,12 @@
+package com.humorous.myapplication.dagger.car;
+
+import dagger.Component;
+
+/**
+ * @author zhangzhiquan
+ * @date 2019-12-29
+ */
+@Component
+public interface CarCommponet {
+  void inject(Car car);
+}
---
+++ MyApplication/app/src/main/java/com/humorous/myapplication/dagger/car/Wheel.java
@@ -0,0 +1,12 @@
+package com.humorous.myapplication.dagger.car;
+
+import javax.inject.Inject;
+
+/**
+ * @author zhangzhiquan
+ * @date 2019-12-29
+ */
+public class Wheel {
+  @Inject
+  public Wheel(){}
+}
