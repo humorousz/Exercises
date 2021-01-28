9de5d0a6db5cd9cbcca050162939689f25026dd2
zhangzhiquan <zhangzhiquan03@kuaishou.com>
2021-01-28 14:54:19 +0800

    升级gradle

--- MyApplication/live-gift-box/src/main/java/com/humorusz/live/giftbox/LiveGifPanelView.java
+++ MyApplication/live-gift-box/src/main/java/com/humorusz/live/giftbox/LiveGifPanelView.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.FrameLayout;
 import android.widget.TabHost;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 /**
