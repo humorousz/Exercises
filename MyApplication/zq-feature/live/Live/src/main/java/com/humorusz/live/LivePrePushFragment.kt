5344363b8050a4fa364a96188269a9beda82c576
zhangzhiquan03 <zhangzhiquan03@kuaishou.com>
2021-09-17 21:53:25 +0800

    preview V1

--- MyApplication/zq-feature/live/Live/src/main/java/com/humorusz/live/LivePrePushFragment.kt
+++ MyApplication/zq-feature/live/Live/src/main/java/com/humorusz/live/LivePrePushFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.humorousz.commonutils.log.Logger
 import com.humorousz.uiutils.helper.ToastUtil
 import com.humorousz.uiutils.view.BaseFragment
 import com.humorusz.live.permission.LivePermissionViewModelFactory
 import com.humorusz.live.permission.LivePushPermissionViewModel
+import io.agora.rtc.IRtcEngineEventHandler
+import io.agora.rtc.RtcEngine
+import io.agora.rtc.RtcEngineConfig
+import io.agora.rtc.video.VideoCanvas
 
 /**
  * @author zhangzhiquan
@@ -16,6 +21,13 @@ import com.humorusz.live.permission.LivePushPermissionViewModel
  *
  */
 class LivePrePushFragment : BaseFragment() {
+  companion object {
+    private const val TAG = "LivePrePushFragment"
+  }
+
+  private var appid = "57072160652442e6ac654a816b6a053b"
+  private var mRootView: ViewGroup? = null
+  private var mRtcEngine: RtcEngine? = null
   private val permissionViewModel by lazy {
     ViewModelProvider(
       this,
@@ -33,17 +45,56 @@ class LivePrePushFragment : BaseFragment() {
 
   override fun initView(root: View?) {
     root?.let {
+      mRootView = root.findViewById(R.id.live_video_container)
       checkPermission()
     }
   }
 
-  private fun checkPermission(){
+  private fun checkPermission() {
     permissionViewModel.getPermission().observe(this) {
       if (it) {
         ToastUtil.showToast(context, "获取权限成功")
+        initRtcEngine()
+        setPreview(mRootView)
       }
     }
     permissionViewModel.requestPermission(activity!!)
+  }
+
+  private fun initRtcEngine() {
+    val config = RtcEngineConfig()
+    config.mContext = activity?.applicationContext
+    config.mAppId = appid
+    config.mEventHandler = object : IRtcEngineEventHandler() {
+      override fun onError(err: Int) {
+        super.onError(err)
+        Logger.d(TAG, "error code:$err");
+      }
+
+      override fun onFirstLocalVideoFrame(width: Int, height: Int, elapsed: Int) {
+        super.onFirstLocalVideoFrame(width, height, elapsed)
+        Logger.d(TAG, "first local frame: $width")
+      }
+    }
+    mRtcEngine = RtcEngine.create(config)
+    mRtcEngine?.setClientRole(IRtcEngineEventHandler.ClientRole.CLIENT_ROLE_BROADCASTER)
+  }
+
+  private fun setPreview(root: ViewGroup?) {
+    root?.let {
+      mRtcEngine?.enableVideo()
+      val surfaceView = RtcEngine.CreateRendererView(context)
+      surfaceView.setZOrderMediaOverlay(true)
+      root.addView(surfaceView)
+      val videoCanvas = VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_HIDDEN, 0)
+      mRtcEngine?.setupLocalVideo(videoCanvas)
+      mRtcEngine?.startPreview()
+    }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    mRtcEngine?.stopPreview()
   }
 
 
