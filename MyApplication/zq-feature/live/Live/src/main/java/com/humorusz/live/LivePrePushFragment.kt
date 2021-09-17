06589b97e013029122baddfd76dc23f239988d7f
zhangzhiquan03 <zhangzhiquan03@kuaishou.com>
2021-09-17 16:57:27 +0800

    添加权限

--- MyApplication/zq-feature/live/Live/src/main/java/com/humorusz/live/LivePrePushFragment.kt
+++ MyApplication/zq-feature/live/Live/src/main/java/com/humorusz/live/LivePrePushFragment.kt
@@ -1,9 +1,14 @@
 package com.humorusz.live
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.humorousz.uiutils.helper.ToastUtil
 import com.humorousz.uiutils.view.BaseFragment
 
 /**
@@ -12,6 +17,15 @@ import com.humorousz.uiutils.view.BaseFragment
  *
  */
 class LivePrePushFragment : BaseFragment() {
+
+  companion object PermissionParams {
+    var PERMISSION_REQ_ID = 22
+    var REQUESTED_PERMISSIONS = arrayOf(
+      Manifest.permission.RECORD_AUDIO,
+      Manifest.permission.CAMERA
+    )
+  }
+
   override fun createView(
     inflater: LayoutInflater?,
     container: ViewGroup?,
@@ -22,8 +36,26 @@ class LivePrePushFragment : BaseFragment() {
 
   override fun initView(root: View?) {
     root?.let {
-
+      if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+        checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID)
+      ) {
+        ToastUtil.showToast(context, "获取权限成功")
+      }
     }
   }
 
+
+  private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
+    activity?.let {
+      if (ContextCompat.checkSelfPermission(
+          activity!!,
+          permission
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        ActivityCompat.requestPermissions(activity!!, REQUESTED_PERMISSIONS, requestCode);
+        return false;
+      }
+    }
+    return true
+  }
 }
