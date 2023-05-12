4425d9e3e8e82f7bba0181f788617d2ad50335c2
zhangzhiquan03 <zhangzhiquan03@kuaishou.com>
2023-05-12 17:54:16 +0800

    scroll Number

--- MyApplication/zq-basic/uiutils/src/main/java/com/humorousz/uiutils/widget/ScrollNumberText.java
+++ MyApplication/zq-basic/uiutils/src/main/java/com/humorousz/uiutils/widget/ScrollNumberText.java
@@ -6,7 +6,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
